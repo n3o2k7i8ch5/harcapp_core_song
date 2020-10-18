4c6a92fc57fef0996d19ea42de4939cadc9d8f6a
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2020-10-18 16:27:58 +0200

    Initial commit

---
+++ lib/song_core.dart
@@ -0,0 +1,21 @@
+abstract class SongCore{
+
+  static const String TAB_CHAR = '   ';
+
+  String get fileName;
+  String get title;
+  List<String> get hidTitles;
+  String get author;
+  String get performer;
+  String get addPers;
+  String get youtubeLink;
+  bool get official;
+
+  List<String> get tags;
+  bool get hasChords;
+
+  String get text;
+  String getChords();
+  int get rate;
+
+}
