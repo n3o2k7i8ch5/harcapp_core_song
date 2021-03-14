154b1a77efd2f9452a4a5a4e0dfb966f6f7383ae
n3o2k7i8ch5 <n3o2k7i8ch5@protonmail.com>
2021-03-14 14:27:51 +0100

    .

--- lib/song_core.dart
+++ lib/song_core.dart
@@ -2,6 +2,21 @@ abstract class SongCore{
 
   static const String TAB_CHAR = '   ';
 
+  static const String PARAM_TITLE = 'title';
+  static const String PARAM_HID_TITLES = 'hid_titles';
+  static const String PARAM_TEXT_AUTHOR = 'text_author';
+  static const String PARAM_PERFORMER = 'performer';
+  static const String PARAM_COMPOSER = 'composer';
+  static const String PARAM_REL_DATE = 'release_date';
+  static const String PARAM_REL_DATE_SHOW_MONTH = 'show_rel_date_month';
+  static const String PARAM_REL_DATE_SHOW_DAY = 'show_rel_date_day';
+  static const String PARAM_YT_LINK = 'yt_link';
+  static const String PARAM_ADD_PERS = 'add_pers';
+  static const String PARAM_TAGS = 'tags';
+  static const String PARAM_REFREN = 'refren';
+  static const String PARAM_PARTS = 'parts';
+
+
   String get fileName;
   String get title;
   List<String> get hidTitles;
